5798bf24f2aee2fa2bd81f45b80cd90013e159fc
David Squire <squiredd@gmail.com>
2018-12-06 21:38:27 -0800

    initial animation

---
+++ Assets/Scripts/Walk.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    Animator animator;
+
+    public float MoveSpeed = 3f;
+	// Use this for initialization
+	void Start ()
+	{
+	    animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Movement();
+	    float move = Input.GetAxis("Horizontal");
+        animator.SetFloat("Speed", move);
+	}
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * MoveSpeed * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.right * MoveSpeed * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, -180);
+        }
+    }
+}
